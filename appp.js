fb08b3ca28c718b84ea790b2a6a4b20d193ae0f9
mercyy-maker <59580006+mercyy-maker@users.noreply.github.com>
2021-04-03 12:16:58 -0700

    Update appp.js
    made changes on appp.js

--- appp.js
+++ appp.js
@@ -1,18 +1,17 @@
-//Oh Yess!
-1 const http = require('http');
-2 
-3 const port = process.env.PORT || 5000;
-4
-5 const server = http.createServer((request, respond) => {
-6         response.statusCode = 200;
-7         response.setHeader('Content-Type', 'text/plain);
-8         response.end('Hello World\n\nCool!');
-9});
-10
-11 server.listen(port, (err) => {
-12         if (err) {
-13                 return console.log('Something went wrong ' , err);
-14         }
-15         console.log('Server Listening on: ${port}');
-16});
-17
+1 // oh Yeah!
+2 const http = require('http');
+3 
+4 const port = process.env.PORT || 5000;
+5
+6 const server = http.createServer((request, response) => {
+7         response.statusCode = 200;
+8         response.setHeader('Content-Type', 'text/plain');
+9         response.end('Hello World\n\n\nYou Guys Rock!);
+10});
+11
+12 server.listening(port, (err) => {
+13         if (err){
+14            return console.log('Somthing is wrong shut the lights');
+15     }
+16     console.log('Server Listening on : ${port}');
+17}
