67429619b98671e105ffd3811969eb5cbce16921
mercyy-maker <lelagom@spu.edu>
2021-04-03 13:47:06 -0700

    updated

--- appp.js
+++ appp.js
@@ -1,17 +1,18 @@
-1 // oh Yeah!
-2 const http = require('http');
-3 
-4 const port = process.env.PORT || 5000;
-5
-6 const server = http.createServer((request, response) => {
-7         response.statusCode = 200;
-8         response.setHeader('Content-Type', 'text/plain');
-9         response.end('Hello World\n\n\nYou Guys Rock!);
-10});
-11
-12 server.listening(port, (err) => {
-13         if (err){
-14            return console.log('Somthing is wrong shut the lights');
-15     }
-16     console.log('Server Listening on : ${port}');
-17}
+const http = require('http');
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((request, response) => {
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/plain');
+        response.end('Hello World\n\n\nYou Guys Rock!');
+});
+
+ server.listen(port, (err) => {
+         if (err){
+           return console.log('Somthing is wrong shut the lights');
+     }
+    console.log('Server Listening on : ${port}');
+         console.log(port);
+
+});
